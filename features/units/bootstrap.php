2bdb0f1a5fb06433e19af9549a39655cec5e86b5
gedi <gediminas.morkevicius@gmail.com>
2013-02-18 10:39:53 +0200

    move unit tests inside features

--- features/units/bootstrap.php
+++ features/units/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
-define('APP_DIR', realpath(__DIR__ . '/../'));
+define('APP_DIR', realpath(__DIR__ . '/../../'));
 
-include APP_DIR.'/functor.php';
+include APP_DIR.'/framework.php';
 
 foreach (glob(APP_DIR.'/services/*.php') as $service) {
     include $service;
