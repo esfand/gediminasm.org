0dc8a371e518c33785c79e60e16af5cb6058c9c8
gedi <gediminas.morkevicius@gmail.com>
2013-06-05 21:33:06 +0300

    fix rss feed generator

--- commands/blog/rss/comments.php
+++ commands/blog/rss/comments.php
@@ -9,16 +9,17 @@ $cmd->setDescription('Generate all blog post comment feeds');
 
 $cmd->setCode(function(InputInterface $in, OutputInterface $out) {
     $RSS2 = <<<EOD
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>Gedi blog post - %title%</title>
     <link>http://gediminasm.org/post/%slug%#comments</link>
     <description>Gediminas Morkevicius comment feed for blog post: %title%</description>
     <language>en-us</language>
     <copyright>Copyright 2013 Gediminas Morkevicius</copyright>
+    <atom:link href="http://gediminasm.org/feeds/%slug%.xml" rel="self" type="application/rss+xml" />
     <lastBuildDate>%build_date%</lastBuildDate>
-    <managingEditor>gediminas.morkevicius@gmail.com</managingEditor>
-    <webMaster>gediminas.morkevicius@gmail.com</webMaster>%items%
+    <managingEditor>gediminas.morkevicius@gmail.com (Gediminas Morkevičius)</managingEditor>
+    <webMaster>gediminas.morkevicius@gmail.com (Gediminas Morkevičius)</webMaster>%items%
   </channel>
 </rss>
 EOD;
@@ -27,6 +28,7 @@ EOD;
     <title>%subject%</title>
     <description>%content%</description>
     <link>http://gediminasm.org/post/%slug%#comments</link>
+    <guid isPermaLink="false">http://gediminasm.org/post/%slug%#%id%</guid>
     <pubDate>%created%</pubDate>
 </item>
 EOD;
@@ -41,15 +43,15 @@ EOD;
         $items = '';
         $out->writeln("Generating RSS comment feed for <comment>{$post['title']}</comment>");
 
-        foreach ($db->all('SELECT subject, content, created FROM comments WHERE post_id = ? ORDER BY created DESC', array($post['id'])) as $comment) {
+        foreach ($db->all('SELECT id, subject, content, created FROM comments WHERE post_id = ? ORDER BY created DESC', array($post['id'])) as $comment) {
             $comment['slug'] = $post['slug'];
             $comment['subject'] = htmlentities($comment['subject'], ENT_XML1);
             $keys = array_map(function($v) {return '%'.$v.'%';}, array_keys($comment));
             $comment['content'] = htmlentities(substr(strip_tags($comment['content']), 0, 300), ENT_XML1).'..';
-            $comment['created'] = date(DATE_RFC822, strtotime($comment['created']));
+            $comment['created'] = date('D, d M Y H:i:s O', strtotime($comment['created']));
             $items .= "\n".str_replace($keys, array_values($comment), $ITEM);
         }
-        $post['build_date'] = date(DATE_RFC822);
+        $post['build_date'] = date('D, d M Y H:i:s O');
         $post['items'] = $items;
         $keys = array_map(function($v) {return '%'.$v.'%';}, array_keys($post));
         file_put_contents($path = $dir."/{$post['slug']}.xml", str_replace($keys, array_values($post), $RSS2));
--- commands/blog/rss/posts.php
+++ commands/blog/rss/posts.php
@@ -9,16 +9,17 @@ $cmd->setDescription('Generated blog rss feed');
 
 $cmd->setCode(function(InputInterface $in, OutputInterface $out) {
     $RSS2 = <<<EOD
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>Gediminas Morkevicius blog posts</title>
     <link>http://gediminasm.org/</link>
     <description>Web blog about programming, innovation and best practices.</description>
     <language>en-us</language>
     <copyright>Copyright 2013 Gediminas Morkevicius</copyright>
+    <atom:link href="http://gediminasm.org/rss.xml" rel="self" type="application/rss+xml" />
     <lastBuildDate>%build_date%</lastBuildDate>
-    <managingEditor>gediminas.morkevicius@gmail.com</managingEditor>
-    <webMaster>gediminas.morkevicius@gmail.com</webMaster>%items%
+    <managingEditor>gediminas.morkevicius@gmail.com (Gediminas Morkevičius)</managingEditor>
+    <webMaster>gediminas.morkevicius@gmail.com (Gediminas Morkevičius)</webMaster>%items%
   </channel>
 </rss>
 EOD;
@@ -42,12 +43,12 @@ EOD;
         $out->writeln("RSS <comment>{$post['title']}</comment>\n");
         $keys = array_map(function($v) {return '%'.$v.'%';}, array_keys($post));
         $post['summary'] = htmlentities(substr(strip_tags($post['summary']), 0, 300)).'..';
-        $post['created'] = date(DATE_RFC822, strtotime($post['created']));
+        $post['created'] = date('D, d M Y H:i:s O', strtotime($post['created']));
         $items .= "\n".str_replace($keys, array_values($post), $ITEM);
     }
     file_put_contents($path = APP_DIR.'/public/rss.xml', str_replace(
         array('%items%', '%build_date%'),
-        array($items, date(DATE_RFC822)),
+        array($items, date('D, d M Y H:i:s O')),
         $RSS2
     ));
     $out->writeln(sprintf("Exported RSS to <info>{$path}</info>\n"));
